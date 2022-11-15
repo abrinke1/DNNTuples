7a5196688747a376d1e27213341dc8afa785293a
Andrew Brinkerhoff <abrinke1@cern.ch>
2022-11-15 15:59:31 -0600

    Kodiak qsub scripts

---
+++ Ntupler/scripts/submitDeepNtuplizer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for mass in 12 15 20 25 30 35 40 45 50 55 60
+do
+    nlines==`wc --lines < test/lists/HToAATo4B_GluGluH_01J_Pt150_M-${mass}.txt`
+    for (( line=0; line<$nlines-1; line++ ))
+    do
+	echo "qsub -N HToAATo4B_GluGluH_01J_Pt150_M-${mass} -v NEVT=-1,IFILE=${line} scripts/runDeepNtuplizer.pbs"
+	qsub -N HToAATo4B_GluGluH_01J_Pt150_M-${mass} -v NEVT=-1,IFILE=${line} scripts/runDeepNtuplizer.pbs
+    done
+done
+
+for qcd in "BGenFilter" "bEnriched"
+do
+    for ht in "500to700" "700to1000" "1000to1500" "1500to2000"
+    do
+	nlines==`wc --lines < test/lists/QCD_${qcd}_HT${ht}.txt`
+	for (( line=0; line<$nlines-1; line++ ))
+	do
+	    echo "qsub -N QCD_${qcd}_HT${ht} -v NEVT=-1,IFILE=${line} scripts/runDeepNtuplizer.pbs"
+	    qsub -N QCD_${qcd}_HT${ht} -v NEVT=-1,IFILE=${line} scripts/runDeepNtuplizer.pbs
+	done
+    done
+done
