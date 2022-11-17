1b48ba12ed33d1a5427e6b4f561b66d466d9a1d7
Andrew Brinkerhoff <abrinke1@cern.ch>
2022-11-17 12:26:13 -0600

    Check for proxy in submission script

--- Ntupler/scripts/submitDeepNtuplizer.sh
+++ Ntupler/scripts/submitDeepNtuplizer.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+echo ""
+echo "To enable grid proxy, have to first run this from the command line:"
+echo "export X509_USER_PROXY=/tmp/x509up_u$(id -u); voms-proxy-init -voms cms -valid 192:00; cp $X509_USER_PROXY x509up"
+echo "Type 'Y' if you have already done this:"
+read -r yes
+echo ""
+
+if [ "$yes" != "Y" ] && [ "$yes" != "y" ] && [ "$yes" != "Yes" ] && [ "$yes" != "yes" ]
+then
+    echo "You typed ${yes}. Quitting."
+    exit 0
+fi
+
+echo "Proceeding to submission:"
+echo ""
+
 for mass in 12 15 20 25 30 35 40 45 50 55 60
 do
     nlines==`wc --lines < test/lists/HToAATo4B_GluGluH_01J_Pt150_M-${mass}.txt`
