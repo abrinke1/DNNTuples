188be2da347de038a80a3fca97daef0491b2f688
chosila <si_sutantawibul1@baylor.edu>
2023-02-20 13:53:20 -0600

    save

--- Ntupler/test/lists/makelisttxt.py
+++ Ntupler/test/lists/makelisttxt.py
@@ -1,6 +1,6 @@
 import os
 
-dirn = '/cms/data/store/user/chosila/SUSY_GluGluH_01J_HToAATo4B_Pt150_mH-70_mA-12_wH-70_wA-70_TuneCP5_13TeV_madgraph_pythia8'
+dirn = '/cms/data/store/user/chosila/SUSY_GluGluH_01J_HToAATo4B_Pt150_mH-70_mA-15_wH-70_wA-50_TuneCP5_13TeV_madgraph_pythia8' #'/cms/data/store/user/chosila/SUSY_GluGluH_01J_HToAATo4B_Pt150_mH-70_mA-12_wH-70_wA-70_TuneCP5_13TeV_madgraph_pythia8'
 rootf = []
 
 with open('tmp.txt', 'w+') as f:
@@ -10,6 +10,4 @@ with open('tmp.txt', 'w+') as f:
             print(line)
             f.write(f'file:{line}\n')
             
-    #for name in dirs:
-   #   print(os.path.join(root, name))
 
